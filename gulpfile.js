41885a0d7a001a1ab58831e791a0445cc2fe7b05
Javier Betancourth <javifond@gmail.com>
2016-12-24 14:15:28 +0100

    MS: Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var notifier = require('node-notifier');
+var server = require('gulp-server-livereload');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+var notify = function (error) {
+  var message = 'In: ',
+      title = 'Error: ';
+
+  if (error.description) {
+    title += error.description;
+  } else if (error.message) {
+    title += error.message;
+  }
+
+  if (error.filename) {
+    var file = error.filename.split('/');
+    message += file[file.length-1];
+  }
+
+  if (error.lineNumber) {
+    message += '\nOn Line: ' + error.lineNumber;
+  }
+
+  notifier.notify({title: title, message: message});
+};
+
+var bundler = watchify(browserify({
+  entries: ['./src/app.jsx'],
+  transform: [reactify],
+  extensions: ['.jsx'],
+  debug: true,
+  cache: {},
+  packageCache: {},
+  fullPaths: true
+}));
+
+function bundle () {
+  return bundler
+    .bundle()
+    .on('error', notify)
+    .pipe(source('js/main.js'))
+    .pipe(gulp.dest('./'))
+}
+
+bundler.on('update', bundle)
+
+gulp.task('build', function () {
+  bundle()
+});
+
+gulp.task('serve', function (done) {
+  gulp.src('')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        filter: function(filePath, cb) {
+          if(/main.js/.test(filePath)) {
+            cb(true)
+          } else if(/style.css/.test(filePath)){
+            cb(true)
+          }
+        }
+      },
+      open: true
+    }));
+});
+
+gulp.task('sass', function () {
+  gulp.src('css/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('css/prod/style.css'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('autoprefixer', function () {
+    gulp.src('css/prod/*.css')
+      .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+      .pipe(gulp.dest('css/dist'));
+});
+
+gulp.task('cssmin', function () {
+  gulp.src('css/dist/*.css')
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css/dist'));
+});
+
+gulp.task('default', ['build', 'sass', 'autoprefixer', 'cssmin']);
+
+// gulp.task('watch', function () {
+//   gulp.watch('./sass/**/*.scss', ['sass', 'autoprefixer', 'cssmin']);
+// });
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return <header>
+		<h1>La vida es de los valientes</h1>
+		<hr />
+    </header>
+  }
+});
