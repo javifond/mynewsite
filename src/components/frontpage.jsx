8d38dda8d4771b52eeee6ead30c630d494c93d4f
Javier Betancourth <javifond@gmail.com>
2016-12-25 11:00:27 +0100

    MS: Frontpage icons change

--- src/components/frontpage.jsx
+++ src/components/frontpage.jsx
@@ -7,11 +7,11 @@ module.exports = React.createClass({
     	className="fp__avatar"/>
 		<h1 className="fp__motto">La vida es de los valientes.</h1>
 		<div className="fp__contact-buttons">
-			<a href="https://www.linkedin.com/in/javier-betancourth-0036a554" target="_blank" rel="noopener"><i className="icon-linkedin-with-circle"></i></a>
-			<a href="https://github.com/javifond" target="_blank" rel="noopener"><i className="icon-github-with-circle"></i></a>
-			<a href="mailto:javifond@gmail.com"><i className="icon-email"></i></a>
+			<a href="https://www.linkedin.com/in/javier-betancourth-0036a554" target="_blank" rel="noopener"><i className="icon-linkedin-square"></i></a>
+			<a href="https://github.com/javifond" target="_blank" rel="noopener"><i className="icon-github"></i></a>
+			<a href="mailto:javifond@gmail.com"><i className="icon-comments-o"></i></a>
 		</div>
-		<div className="fp__location"><i className="icon-location-pin"></i> Malaga - Spain</div>
+		<div className="fp__location"><i className="icon-street-view"></i> Malaga - Spain</div>
     </header>
   }
 });
