a837f4172823c480784118f90856e027282bff49
Javier Betancourth <javifond@gmail.com>
2016-12-25 18:24:46 +0100

    MS: Responsive images

--- src/components/like.jsx
+++ src/components/like.jsx
@@ -7,15 +7,27 @@ module.exports = React.createClass({
 			<h2 className="like__title">I like to...</h2>
 			<div>
 				<figure className="photo photo--beer">
-					<img className="photo__img" src="https://s.gravatar.com/avatar/e4664b531ad286257f3503cf72adc15d?s=300" alt="My Avatar"/>
+					<img className="photo__img"
+						srcSet="./css/dist/assets/img/beer-480.jpg  480w,
+						./css/dist/assets/img/beer-640.jpg  640w"
+						sizes="(min-width: 768px) 100vw, 50vw"
+						src="./css/dist/assets/img/beer-480.jpg" alt="I love beer"/>
 					<figcaption className="photo__title">Drink beer with friends...</figcaption>
 				</figure>
 				<figure className="photo photo--travel">
-					<img className="photo__img" src="https://s.gravatar.com/avatar/e4664b531ad286257f3503cf72adc15d?s=300" alt="My Avatar"/>
+					<img className="photo__img"
+						srcSet="./css/dist/assets/img/places-480.jpg  480w,
+						./css/dist/assets/img/places-640.jpg  640w"
+						sizes="(min-width: 768px) 100vw, 50vw"
+						src="./css/dist/assets/img/places-480.jpg" alt="I like to travel"/>
 					<figcaption className="photo__title">Fly with the air...</figcaption>
 				</figure>
 				<figure className="photo photo--food">
-					<img className="photo__img" src="https://s.gravatar.com/avatar/e4664b531ad286257f3503cf72adc15d?s=300" alt="My Avatar"/>
+					<img className="photo__img"
+						srcSet="./css/dist/assets/img/food-480.jpg  480w,
+						./css/dist/assets/img/food-640.jpg  640w"
+						sizes="(min-width: 768px) 100vw, 50vw"
+						src="./css/dist/assets/img/food-480.jpg" alt="I love food and nice views"/>
 					<figcaption className="photo__title">Good food & nice views...</figcaption>
 				</figure>
 			</div>
